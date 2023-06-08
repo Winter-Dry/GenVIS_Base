578d5b6f0b725255d7d4a6ccf4f11ecf7afeab71
jzl <1650820702@qq.com>
2023-06-08 08:43:04 +0800

    code debug

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 python train_net_genvis.py --num-gpus 4 \
   --config-file configs/genvis/youtubevis_2021/genvis_R50_bs8_online.yaml \
-  MODEL.WEIGHTS vita_r50_ytvis21.pth \
+  MODEL.WEIGHTS vita_r50_ytvis2021.pth \
   OUTPUT_DIR output/genvis_R50_bs8_online \
   WANDB.ENABLED False \
   WANDB.NAME genvis_R50_bs8_online
