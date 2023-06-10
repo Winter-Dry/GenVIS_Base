ec266e85b651f11211fa4b895593d5bb3bf61efc
jzl <1650820702@qq.com>
2023-06-10 21:23:04 +0800

    code debug

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 python train_net_genvis.py --num-gpus 4 \
   --config-file configs/genvis/youtubevis_2021/genvis_R50_bs8_online.yaml \
   MODEL.WEIGHTS vita_r50_ytvis2021.pth \
-  OUTPUT_DIR output/genvis_R50_bs32_online_feature_fusion_v0 \
+  OUTPUT_DIR output/genvis_R50_bs16_online_feature_fusion_v0 \
+  SOLVER.IMS_PER_BATCH 16 \
+  SOLVER.STEPS '(25000,)' \
+  SOLVER.MAX_ITER 45000 \
   WANDB.ENABLED True \
-  WANDB.NAME genvis_R50_bs32_online_feature_fusion_v0
+  WANDB.NAME genvis_R50_bs16_online_feature_fusion_v0
