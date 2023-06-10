d78d34bf0fefd96e62cbde010f5a97cf293d222b
jzl <1650820702@qq.com>
2023-06-10 20:42:04 +0800

    code debug

--- genvis/genvis_model.py
+++ genvis/genvis_model.py
@@ -1,5 +1,6 @@
 import math
 
+import numpy as np
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -14,21 +15,27 @@ from .modeling.genvis_criterion import GenvisSetCriterion
 from .modeling.genvis_matcher import GenvisHungarianMatcher
 from .modeling.genvis import GenVIS
 
+
 @META_ARCH_REGISTRY.register()
 class Genvis(Vita):
 
     @configurable
     def __init__(
-        self,
-        len_clip_window: int,
-        genvis_criterion: nn.Module,
-        **kwargs,
+            self,
+            len_clip_window: int,
+            genvis_criterion: nn.Module,
+            **kwargs,
     ):
         super().__init__(**kwargs)
 
         self.len_clip_window = len_clip_window
         self.genvis_criterion = genvis_criterion
         self.freeze_detector = kwargs["freeze_detector"]
+
+        self.diff_conv = nn.ModuleDict()
+        for k, v in self.backbone.output_shape().items():
+            self.diff_conv[k] = nn.Conv2d(in_channels=self.num_frames * v.channels, out_channels=v.channels,
+                                          kernel_size=1, stride=1)
 
     @classmethod
     def from_config(cls, cfg):
@@ -39,9 +46,9 @@ class Genvis(Vita):
 
         # loss weights
         class_weight = cfg.MODEL.MASK_FORMER.CLASS_WEIGHT
-        dice_weight  = cfg.MODEL.MASK_FORMER.DICE_WEIGHT
-        mask_weight  = cfg.MODEL.MASK_FORMER.MASK_WEIGHT
-        sim_weight   = cfg.MODEL.VITA.SIM_WEIGHT
+        dice_weight = cfg.MODEL.MASK_FORMER.DICE_WEIGHT
+        mask_weight = cfg.MODEL.MASK_FORMER.MASK_WEIGHT
+        sim_weight = cfg.MODEL.VITA.SIM_WEIGHT
 
         genvis_matcher = GenvisHungarianMatcher(
             cost_class=class_weight,
@@ -67,11 +74,11 @@ class Genvis(Vita):
 
         num_classes = rets["sem_seg_head"].num_classes
         genvis_criterion = GenvisSetCriterion(
-            num_classes, 
-            matcher=genvis_matcher, 
+            num_classes,
+            matcher=genvis_matcher,
             weight_dict=genvis_weight_dict,
             eos_coef=cfg.MODEL.VITA.NO_OBJECT_WEIGHT,
-            losses=genvis_losses, 
+            losses=genvis_losses,
             num_points=cfg.MODEL.MASK_FORMER.TRAIN_NUM_POINTS,
             oversample_ratio=cfg.MODEL.MASK_FORMER.OVERSAMPLE_RATIO,
             importance_sample_ratio=cfg.MODEL.MASK_FORMER.IMPORTANCE_SAMPLE_RATIO,
@@ -95,7 +102,7 @@ class Genvis(Vita):
         features = self.backbone(images.tensor)
 
         BT = len(images)
-        T = self.num_frames if self.training else BT 
+        T = self.num_frames if self.training else BT
         B = BT // T
 
         outputs, frame_queries, mask_features = self.sem_seg_head(features)
@@ -114,7 +121,7 @@ class Genvis(Vita):
 
         if self.freeze_detector:
             losses = dict()
-        
+
         for k in list(losses.keys()):
             if k in self.criterion.weight_dict:
                 losses[k] *= self.criterion.weight_dict[k]
@@ -126,38 +133,39 @@ class Genvis(Vita):
 
         frame_targets = self.split_frame_targets(frame_targets, B)
         video_targets = self.split_video_targets(video_targets)
-        fg_indices    = self.split_fg_indices(fg_indices, B)
+        fg_indices = self.split_fg_indices(fg_indices, B)
 
         frame_queries = frame_queries.reshape(L, B, T, fQ, C)
         frame_queries = frame_queries.split(self.len_clip_window, dim=2)
         mask_features = mask_features.split(self.len_clip_window, dim=1)
-        
+
         pre_memory = {"k": [], "v": []}
 
         prev_clip_indices = None
         prev_aux_clip_indices = None
-        output_q = self.vita_module.query_feat.weight.unsqueeze(1).repeat(1, L*B, 1) # cQ, LB, C
-        
+        output_q = self.vita_module.query_feat.weight.unsqueeze(1).repeat(1, L * B, 1)  # cQ, LB, C
+
         for c_i in range(num_clips):
-            clip_targets  = video_targets[c_i]
+            clip_targets = video_targets[c_i]
             frame_targets_per_clip = frame_targets[c_i]
             frame_queries_per_clip = frame_queries[c_i]
             mask_features_per_clip = mask_features[c_i]
             fg_indices_per_clip = fg_indices[c_i]
 
-            vita_outputs, output_q = self.vita_module(frame_queries_per_clip.flatten(1,2), pre_memory, output_q)
-            vita_outputs["pred_masks"] = torch.einsum("lbqc,btchw->lbqthw", vita_outputs["pred_mask_embed"], mask_features_per_clip)
+            vita_outputs, output_q = self.vita_module(frame_queries_per_clip.flatten(1, 2), pre_memory, output_q)
+            vita_outputs["pred_masks"] = torch.einsum("lbqc,btchw->lbqthw", vita_outputs["pred_mask_embed"],
+                                                      mask_features_per_clip)
             for out in vita_outputs["aux_outputs"]:
                 out["pred_masks"] = torch.einsum("lbqc,btchw->lbqthw", out["pred_mask_embed"], mask_features_per_clip)
 
             genvis_loss_dict, out_clip_indices, aux_clip_indices_list = self.genvis_criterion(
-                                                                            vita_outputs, 
-                                                                            clip_targets, 
-                                                                            frame_targets_per_clip, 
-                                                                            fg_indices_per_clip, 
-                                                                            prev_clip_indices, 
-                                                                            prev_aux_clip_indices
-                                                                        )
+                vita_outputs,
+                clip_targets,
+                frame_targets_per_clip,
+                fg_indices_per_clip,
+                prev_clip_indices,
+                prev_aux_clip_indices
+            )
             genvis_weight_dict = self.genvis_criterion.weight_dict
 
             loss_dict_keys = list(genvis_loss_dict.keys())
@@ -176,21 +184,47 @@ class Genvis(Vita):
             prev_aux_clip_indices = aux_clip_indices_list
         return losses
 
+    def diff_feature_fusion(self, features, alpha1=0.5, alpha2=0.5):  # (B*T,C,H,W)
+        BT = features.values()[0].shape[0]
+        T = self.num_frames if self.training else BT
+        B = BT // T
+
+        for k, f in features.items():
+            f = f.resize(B, T, *f.shape[1:])  # (B,T,C,H,W)
+            diff_features = []
+            for i in range(f.shape[1]):
+                # indices = list(range(f.shape[1]))
+                # indices.pop(i)
+
+                start_ind = max(0, i - self.num_frames)
+                end_ind = min(f.shape[1], i + 1 + self.num_frames)
+
+                diff_features.append(torch.cat(
+                    [f[:, start_ind:i, ...] - f[:, i, ...][:, None],
+                     f[:, i, ...][:, None] - f[:, i + 1:end_ind, ...]],
+                    dim=1))
+
+            diff_features = torch.stack(diff_features, dim=1).reshape(BT, -1, *f.shape[
+                                                                               -2:])  # (B,T,T-1,C,H,W) -> (B*T,(T-1)*C,H,W)
+            features[k] = alpha1 * f.resize(BT, -1, *f.shape[-2:]) + alpha2 * self.diff_feature_conv[k](diff_features)
+
+        return diff_features
+
     def split_frame_targets(self, frame_targets, batch_size):
         T = self.num_frames
         W = self.len_clip_window
         num_clips = T // W
 
-        frame_targets = [frame_targets[b_i*T:(b_i+1)*T] for b_i in range(batch_size)]
+        frame_targets = [frame_targets[b_i * T:(b_i + 1) * T] for b_i in range(batch_size)]
 
         frame_targets_splits = dict()
         for frame_targets_per_batch in frame_targets:
             for clip_idx in range(num_clips):
                 if not clip_idx in frame_targets_splits:
                     frame_targets_splits[clip_idx] = []
-        
-                frame_targets_splits[clip_idx] += frame_targets_per_batch[clip_idx*W:(clip_idx+1)*W]
-        
+
+                frame_targets_splits[clip_idx] += frame_targets_per_batch[clip_idx * W:(clip_idx + 1) * W]
+
         return list(frame_targets_splits.values())
 
     def split_video_targets(self, clip_targets):
@@ -198,18 +232,18 @@ class Genvis(Vita):
 
         clip_target_splits = dict()
         for targets_per_video in clip_targets:
-            labels = targets_per_video["labels"] # Ni (number of instances)
-            ids = targets_per_video["ids"] # Ni, T 
-            masks = targets_per_video["masks"] # Ni, T, H, W
-            frame_idx = targets_per_video["frame_idx"] # T
+            labels = targets_per_video["labels"]  # Ni (number of instances)
+            ids = targets_per_video["ids"]  # Ni, T
+            masks = targets_per_video["masks"]  # Ni, T, H, W
+            frame_idx = targets_per_video["frame_idx"]  # T
 
             masks_splits = masks.split(clip_len, dim=1)
             ids_splits = ids.split(clip_len, dim=1)
 
             prev_valid = torch.zeros_like(labels).bool()
             for clip_idx, (_masks, _ids) in enumerate(zip(masks_splits, ids_splits)):
-                valid_inst = _masks.sum(dim=(1,2,3)) > 0.
-                new_inst   = (prev_valid == False) & (valid_inst == True)
+                valid_inst = _masks.sum(dim=(1, 2, 3)) > 0.
+                new_inst = (prev_valid == False) & (valid_inst == True)
 
                 if not clip_idx in clip_target_splits:
                     clip_target_splits[clip_idx] = []
@@ -217,8 +251,8 @@ class Genvis(Vita):
                 clip_target_splits[clip_idx].append(
                     {
                         "labels": labels, "ids": _ids, "masks": _masks,
-                        "video_len": targets_per_video["video_len"], 
-                        "frame_idx": frame_idx[clip_idx*clip_len:(clip_idx+1)*clip_len],
+                        "video_len": targets_per_video["video_len"],
+                        "frame_idx": frame_idx[clip_idx * clip_len:(clip_idx + 1) * clip_len],
                         "valid_inst": valid_inst,
                         "new_inst": new_inst,
                     }
@@ -236,13 +270,13 @@ class Genvis(Vita):
 
         fg_indices_splits = []
         for L_i in range(L):
-            fg_indices_Li = [fg_indices[L_i][b_i*T:(b_i+1)*T] for b_i in range(batch_size)]
+            fg_indices_Li = [fg_indices[L_i][b_i * T:(b_i + 1) * T] for b_i in range(batch_size)]
             fg_indices_Li_splits = dict()
             for b_i in range(batch_size):
                 for clip_idx in range(num_clips):
                     if not clip_idx in fg_indices_Li_splits:
                         fg_indices_Li_splits[clip_idx] = []
-                    fg_indices_Li_splits[clip_idx] += fg_indices_Li[b_i][clip_idx*W:(clip_idx+1)*W]
+                    fg_indices_Li_splits[clip_idx] += fg_indices_Li[b_i][clip_idx * W:(clip_idx + 1) * W]
             fg_indices_splits.append(fg_indices_Li_splits)
 
         fg_indices_splits_clips = []
@@ -251,7 +285,7 @@ class Genvis(Vita):
             for L_i in range(L):
                 per_clip.append(fg_indices_splits[L_i][clip_idx])
             fg_indices_splits_clips.append(per_clip)
-            
+
         return fg_indices_splits_clips
 
     def inference(self, batched_inputs):
@@ -268,9 +302,9 @@ class Genvis(Vita):
         mask_cls, mask_embed = [], []
         pre_memory = {"k": [], "v": []}
 
-        output_q = self.vita_module.query_feat.weight.unsqueeze(1).repeat(1, 1, 1) # cQ, LB, C
+        output_q = self.vita_module.query_feat.weight.unsqueeze(1).repeat(1, 1, 1)  # cQ, LB, C
         for i in range(math.ceil(num_frames / self.len_clip_window)):
-            images = batched_inputs["image"][i*self.len_clip_window : (i+1)*self.len_clip_window]
+            images = batched_inputs["image"][i * self.len_clip_window: (i + 1) * self.len_clip_window]
             images = [(x.to(self.device) - self.pixel_mean) / self.pixel_std for x in images]
             images = ImageList.from_tensors(images, self.size_divisibility)
 
@@ -283,8 +317,8 @@ class Genvis(Vita):
             _mask_features = self.vita_module.vita_mask_features(_mask_features)
             mask_features.append(_mask_features)  # T', C, H, W
 
-            mask_cls.append(vita_outputs["pred_logits"][-1])       # 1, cQ, K+1
-            mask_embed.append(vita_outputs["pred_mask_embed"][-1]) # 1, cQ, C
+            mask_cls.append(vita_outputs["pred_logits"][-1])  # 1, cQ, K+1
+            mask_embed.append(vita_outputs["pred_mask_embed"][-1])  # 1, cQ, C
 
             # update memory
             pre_memory["k"].append(vita_outputs["pre_memory"]["k"])
@@ -292,20 +326,21 @@ class Genvis(Vita):
             del vita_outputs
 
         interim_size = images.tensor.shape[-2:]
-        image_size   = images.image_sizes[0]  # image size without padding after data augmentation
+        image_size = images.image_sizes[0]  # image size without padding after data augmentation
 
         out_height = batched_inputs.get("height", image_size[0])  # raw image size before data augmentation
-        out_width  = batched_inputs.get("width", image_size[1])
+        out_width = batched_inputs.get("width", image_size[1])
 
         del outputs, images, batched_inputs
 
-        mask_cls   = torch.cat(mask_cls)   # NUM_CLIP, cQ, K+1
-        mask_embed = torch.cat(mask_embed) # NUM_CLIP, cQ, C
+        mask_cls = torch.cat(mask_cls)  # NUM_CLIP, cQ, K+1
+        mask_embed = torch.cat(mask_embed)  # NUM_CLIP, cQ, C
 
-        labels = torch.arange(self.sem_seg_head.num_classes, device=self.device).unsqueeze(0).repeat(self.num_queries, 1).flatten(0, 1)
+        labels = torch.arange(self.sem_seg_head.num_classes, device=self.device).unsqueeze(0).repeat(self.num_queries,
+                                                                                                     1).flatten(0, 1)
         num_topk = self.test_topk_per_image
 
-        scores = F.softmax(mask_cls, dim=-1)[:, :, :-1] # NUM_CLIP, cQ, K
+        scores = F.softmax(mask_cls, dim=-1)[:, :, :-1]  # NUM_CLIP, cQ, K
         scores_per_video, _ = scores.max(dim=0)
         scores_per_video, topk_indices = scores_per_video.flatten().topk(num_topk, sorted=False)
 
@@ -315,10 +350,10 @@ class Genvis(Vita):
         mask_embed = mask_embed[:, topk_indices]
 
         masks_per_video = []
-        numerator   = torch.zeros(len(topk_indices), dtype=torch.float, device=self.device)
+        numerator = torch.zeros(len(topk_indices), dtype=torch.float, device=self.device)
         denominator = torch.zeros(len(topk_indices), dtype=torch.float, device=self.device)
 
-        for i in range(math.ceil(num_frames/self.len_clip_window)):
+        for i in range(math.ceil(num_frames / self.len_clip_window)):
             mask_pred = torch.einsum("qc,tchw->qthw", mask_embed[i], mask_features[i])
 
             # upsample masks
@@ -327,14 +362,14 @@ class Genvis(Vita):
                 size=interim_size,
                 mode="bilinear",
                 align_corners=False,
-            ) # cQ, T, H, W
+            )  # cQ, T, H, W
 
             mask_pred = mask_pred[:, :, : image_size[0], : image_size[1]]
 
             interim_mask_soft = mask_pred.sigmoid()
             interim_mask_hard = interim_mask_soft > 0.5
 
-            numerator   += (interim_mask_soft.flatten(1) * interim_mask_hard.flatten(1)).sum(1)
+            numerator += (interim_mask_soft.flatten(1) * interim_mask_hard.flatten(1)).sum(1)
             denominator += interim_mask_hard.flatten(1).sum(1)
 
             mask_pred = F.interpolate(
@@ -343,7 +378,7 @@ class Genvis(Vita):
 
             masks_per_video.append(mask_pred.to(to_store))
 
-        masks_per_video   = torch.cat(masks_per_video, dim=1).cpu()
+        masks_per_video = torch.cat(masks_per_video, dim=1).cpu()
         scores_per_video *= (numerator / (denominator + 1e-6))
 
         processed_results = {
