5fd2382201d8aecb4e1e88256d6ead9fbd978023
jzl <1650820702@qq.com>
2023-06-12 08:39:03 +0800

    add ytvis2021mini dataset

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 python train_net_genvis.py --num-gpus 4 \
   --config-file configs/genvis/youtubevis_2021/genvis_R50_bs8_online.yaml \
   MODEL.WEIGHTS vita_r50_ytvis2021.pth \
-  OUTPUT_DIR output/genvis_R50_bs16_online_feature_fusion_v0 \
+  OUTPUT_DIR output/genvis_yt21mini_R50_bs8_online_feature_fusion_v0 \
   SOLVER.IMS_PER_BATCH 8 \
   SOLVER.STEPS '(50000,)' \
   SOLVER.MAX_ITER 90000 \
   WANDB.ENABLED True \
-  WANDB.NAME genvis_R50_bs16_online_feature_fusion_v0
+  WANDB.NAME genvis_yt21mini_R50_bs8_online_feature_fusion_v0
